1863037946ca1a0f106aaf75b82be85139077825
Joaquin <joaquinfrommer@gmail.com>
2022-05-26 01:03:38 -0400

    backend hot fix 1.4.1 fix reserved word

--- backend/src/dynamoDB.js
+++ backend/src/dynamoDB.js
@@ -6,10 +6,10 @@ const client =  new DynamoDB.DocumentClient();
 //Adds user, takes in a user object
 //User object:
 //{name: 'name', phone: 'phone #'}
-export async function addUser(user) {
+export function addUser(user) {
     const addUserResult = () => {
         try {
-            await client.put({
+            client.put({
                 TableName : tableName,
                 Item: user
             }).promise()
@@ -23,14 +23,14 @@ export async function addUser(user) {
 }
 
 //Deletes user from the dynamodb table, takes in a correctly formatted phone string
-export async function delUser(phone) {
+export function delUser(phone) {
     let params = {
         TableName: tableName,
         Key: {Phone: phone}
     };
     const delResult = () => {
         try {
-            await this.client.delete(params).promise()
+            this.client.delete(params).promise()
             return true;
         } catch (e) {
             console.log(e);
@@ -41,11 +41,11 @@ export async function delUser(phone) {
 }
 
 //Gets a list of all users signed up for the service 
-export async function allUsers() {
+export function allUsers() {
     let params = {TableName: tableName};
     const scanResult = () => {
         try {
-            const data = await this.client.scan(params).promise()
+            const data = this.client.scan(params).promise()
             return data;
         } catch (e) {
             console.log(e);
