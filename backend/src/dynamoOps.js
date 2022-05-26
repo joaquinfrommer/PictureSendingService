bced2118adceccbff146f244345a25c4359539b4
Joaquin <joaquinfrommer@gmail.com>
2022-05-26 01:25:17 -0400

    backend hot fix 1.5.2 fix Dynamo import issue

--- backend/src/dynamoOps.js
+++ backend/src/dynamoOps.js
@@ -1,4 +1,4 @@
-import { DynamoDB } from 'aws-sdk'
+import DynamoDB from 'aws-sdk'
 
 const tableName = process.env.TABLE_NAME || 'PicSenderUserTable';
 const client =  new DynamoDB.DocumentClient();
