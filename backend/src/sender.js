5999d745eda64242f0b2374f2cad7c910350c3af
Joaquin <joaquinfrommer@gmail.com>
2022-06-07 14:42:04 -0700

    backend 3.1.16 fixing stupid twilio

--- backend/src/sender.js
+++ backend/src/sender.js
@@ -8,6 +8,7 @@ const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const twlPhone = process.env.TW_PHONE
 const twClient = new twilio(accountSid, authToken);
+const twilio_endpoint = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`
 
 async function getImage() {
     try {
@@ -20,55 +21,80 @@ async function getImage() {
     }
 }
 
+const dog_image = await getImage();
+
 function send_error(e) {
     console.log(e);
 }
 
-async function send_picture(user, image, callback) {
-    const user_name = user.Name;
-    const user_phone = user.Phone;
-    try{
-        const message = await twClient.messages.create({
-            body: `Hi ${user_name}! Enjoy your dog :)`,
-            from: twlPhone,
-            mediaUrl: [image],
-            to: user_phone
-          });
-
-        console.log(message.sid);
-        return callback(null, {result: 'success'});
-        // return message;
-    } catch (e) {
-        send_error(e);
-        return callback("error");
-        // return e
-    }
-}
-
-// async function send_picture() {
-    
+// async function send_picture(user, image) {
+//     const user_name = user.Name;
+//     const user_phone = user.Phone;
+//     try{
+//         const message = await twClient.messages.create({
+//             body: `Hi ${user_name}! Enjoy your dog :)`,
+//             from: twlPhone,
+//             mediaUrl: [image],
+//             to: user_phone
+//           });
+//         callback(null, {result: 'success'});
+//         return message;
+//     } catch (e) {
+//         callback("error");
+//         return e;
+//     }
 // }
 
-async function send_to_user(user, callback) {
-    const dog_image = await getImage();
+async function send_picture(user, image) {
+    const user_name = user.Name;
+    const user_phone = user.Phone;
+    const contents = {
+        body: `Hi ${user_name}! Enjoy your dog :)`,
+        from: twlPhone,
+        mediaUrl: [image],
+        to: user_phone
+    };
+    const auth = { 
+        auth: {
+            username: accountSid,
+            password: authToken
+        }
+    }
+    
+    try{
+        const res = await axios.post(twilio_endpoint, contents, auth);
+        const message = res.data;
+        return message;
+    } catch (e) {
+        return e;
+    }
+    
+    return "Should not be reachable!";
+}
+
+async function send_to_user(user) {
     if (!dog_image) {
         send_error("Image not available");
         return;
     }
     console.log("Using image:", dog_image);
-    const message = await send_picture(user, dog_image, callback);
-    console.log(message);
-    return ;
+    const message = await send_picture(user, dog_image);
+    
+    if (message.sid) {
+        console.log(message.sid);
+    } else {
+        send_error(message);
+    }    
 }
 
-async function send(event, context, callback) {
+async function send() {
     const users = await allUsers();
     if (!users) {
         send_error("DynamoDB Issue!");
         return ;
     }
     console.log("Attempting to send to users in list", users);
-    users.forEach(user => send_to_user(user, callback));
+    users.forEach(send_to_user);
     return ; 
 }
 
